f14594215aaa4a846ba3f00082539145adbdd7f1
florian lahitte <lahitte.florian@gmail.com>
2017-01-09 15:58:38 +0100

    Add carriwave and cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable,:omniauthable, omniauth_providers: [:facebook]
+  mount_uploader :photo, PhotoUploader
+
  def self.find_for_facebook_oauth(auth)
      user_params = auth.to_h.slice(:provider, :uid)
      user_params.merge! auth.info.slice(:email, :first_name, :last_name)
